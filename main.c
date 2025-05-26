2fa726a71ec6811d318bfc4d6b0a5e5646892542
Adam Dziwiszek <adziwiszek1@gmail.com>
2025-05-26 23:59:31 +0200

    improved snake movement

--- main.c
+++ main.c
@@ -8,6 +8,42 @@
 #include <poll.h>
 
 
+void clear_screen() {
+  printf("\033[H\033[J");
+}
+    
+struct timespec ts = {
+  .tv_sec = 0,
+  .tv_nsec = 300000000L  // 300 ms
+};
+
+void set_input_mode(int enable) {
+  static struct termios oldt, newt;
+
+  if (!enable) {
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  } else {
+    tcgetattr(STDIN_FILENO, &oldt); 
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+  }
+}
+
+void flush_stdin() {
+  int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+  char tmp;
+  while (read(STDIN_FILENO, &tmp, 1) == 1) { }
+  fcntl(STDIN_FILENO, F_SETFL, flags);
+}
+
+void exit_program(char* message) {
+  printf("Message: %s\n", message);
+  set_input_mode(0);
+  exit(1);
+}
+
 // ============================================================================
 
 typedef enum {
@@ -49,7 +85,7 @@ void init_empty_board(Board* b, int size_x, int size_y) {
   b->map = malloc(size_y * sizeof(BoardField*));
   if (b->map == NULL) {
     fprintf(stderr, "Malloc error for rows\n");
-    exit(1);
+    exit_program("Malloc error");
   }
   
   for(int i = 0; i < size_y; i++) {
@@ -57,10 +93,10 @@ void init_empty_board(Board* b, int size_x, int size_y) {
     if (b->map[i] == NULL) {
       fprintf(stderr, "Mallloc error for row: %d\n", i);
       for(int j = 0; j < i; j++) {
-        free(b->map[j]);
+        exit_program("Malloc error");
       }
       free(b->map);
-      exit(1);
+      exit_program("Malloc error");
     }
   }
   
@@ -87,6 +123,7 @@ typedef enum {
   DOWN,
   LEFT,
   RIGHT,
+  NIL,
 } Dir;
 
 typedef struct {
@@ -98,62 +135,115 @@ typedef struct {
   Point start;
   Point end;
   Dir direction;
+  Dir **dirMap;
 } SnakeData;
 
-void init_snake(SnakeData* snake) {
+void init_snake(SnakeData* snake, Board* b) {
   snake->start = (Point){.x = 5, .y = 0};
   snake->end = (Point){.x = 0, .y = 0};
   snake->direction = RIGHT;
-}
 
-void init_snake_on_board(Board* b, SnakeData* s) {
-  Point snake_body = s->end;
-  while(snake_body.x != s->start.x || snake_body.y != s->start.y) {
+  snake->dirMap = malloc(b->size_y * sizeof(Dir*));
+  if(snake->dirMap == NULL) {
+    fprintf(stderr, "Malloc error for rows\n");
+    exit_program("Malloc error");
+  }
+  
+  for(int i = 0; i < b->size_y; i++) {
+    snake->dirMap[i] = malloc(b->size_x * sizeof(Dir));
+    if (b->map[i] == NULL) {
+      fprintf(stderr, "Mallloc error for row: %d\n", i);
+      for(int j = 0; j < i; j++) {
+        free(snake->dirMap[j]);
+      }
+      free(snake->dirMap);
+      exit_program("Malloc error");
+    }
+  }
+  
+  for(int i = 0; i < b->size_y; i++) {
+    for(int j = 0; j < b->size_x; j++) {
+      snake->dirMap[i][j] = NIL;
+    }
+  }
+
+  Point snake_body = snake->end;
+  while(snake_body.x != snake->start.x || snake_body.y != snake->start.y) {
+    snake->dirMap[snake_body.y][snake_body.x] = snake->direction;  
     b->map[snake_body.y][snake_body.x] = Snake;  
-    switch(s->direction) {
+    switch(snake->direction) {
       case RIGHT:
         snake_body.x++;
         break;
       default:
-        printf("unsupported direction %c\n", s->direction);
+        printf("unsupported direction %c\n", snake->direction);
         //exit(1);
     }
   }
+
+  b->map[snake_body.y][snake_body.x] = Snake;
+  snake->dirMap[snake_body.y][snake_body.x] = snake->direction; 
 }
 
-void update_snake(Board* b, SnakeData* s) {
+void free_snake(SnakeData* s, int board_size_y) {
+  if(s->dirMap != NULL) {
+    for(int i = 0; i < board_size_y; i++) {
+      free(s->dirMap[i]);
+    }
+    free(s->dirMap);
+  }
+}
+
+void update_snake(SnakeData* s, Board* b) {
+  // change the direction at the turn
+  s->dirMap[s->start.y][s->start.x] = s->direction; 
   // snake head
   switch(s->direction) {
     case RIGHT:
-      b->map[s->start.y][(s->start.x)++] = Snake;
+      s->start.x++;
       break;
     case LEFT:
-      b->map[s->start.y][(s->start.x)--] = Snake;
+      s->start.x--;
       break;
     case DOWN:
-      b->map[(s->start.y)++][s->start.x] = Snake;
+      s->start.y++;
       break;
     case UP:
-      b->map[(s->start.y)--][s->start.x] = Snake;
+      s->start.y--;
       break;
     default:
       printf("unsupported direction %c\n", s->direction);
   }
+  if(s->start.x >= b->size_x || s->start.x < 0 || 
+     s->start.y >= b->size_y || s->start.y < 0) {
+    // we went out of bounds
+    exit_program("snake went out of bounds");
+  }
+  s->dirMap[s->start.y][s->start.x] = s->direction; 
+  b->map[s->start.y][s->start.x] = Snake;
 
   // snake butt
- if(s->end.x < b->size_x && b->map[s->end.y][s->end.x+1] == Snake) {
-    // check right
-    b->map[s->end.y][s->end.x++] = Empty;
-  } else if(s->end.x < b->size_x && b->map[s->end.y][s->end.x-1] == Snake) {
-    // check left
-    b->map[s->end.y][s->end.x--] = Empty;
-  } else if(s->end.y+1 < b->size_y && b->map[s->end.y+1][s->end.x] == Snake) {
-    // check down
-    b->map[s->end.y++][s->end.x] = Empty;
-  } else if(s->end.y < b->size_y && b->map[s->end.y-1][s->end.x] == Snake) {
-    // check up
-    b->map[s->end.y--][s->end.x] = Empty;
+  Point old_end = s->end;
+  b->map[s->end.y][s->end.x] = Empty;
+  switch(s->dirMap[s->end.y][s->end.x]) {
+    case RIGHT:
+      s->end.x++;
+      break;
+    case LEFT:
+      s->end.x--;
+      break;
+    case DOWN:
+      s->end.y++;
+      break;
+    case UP:
+      s->end.y--;
+      break;
+    case NIL:
+      break;
+    default:
+      printf("unsupported direction %c\n", s->direction);
   }
+  s->dirMap[old_end.y][old_end.x] = NIL;
 }
 
 void set_snake_direction(char c, SnakeData* s) {
@@ -175,37 +265,37 @@ void set_snake_direction(char c, SnakeData* s) {
   }
 }
 
-// ============================================================================
-
-void clear_screen() {
-  printf("\033[H\033[J");
-}
-    
-struct timespec ts = {
-  .tv_sec = 0,
-  .tv_nsec = 300000000L  // 300 ms
-};
-
-void set_input_mode(int enable) {
-  static struct termios oldt, newt;
-
-  if (!enable) {
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  } else {
-    tcgetattr(STDIN_FILENO, &oldt); 
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO); 
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+void print_snake_directions(SnakeData* s, Board* b) {
+  for(int i = 0; i < b->size_y; i++) {
+    for(int j = 0; j < b->size_x; j++) {
+      char c;
+      switch(s->dirMap[i][j]) {
+        case RIGHT:
+          c = 'R';
+          break;
+        case LEFT:
+          c = 'L';
+          break;
+        case DOWN:
+          c = 'D';
+          break;
+        case UP:
+          c = 'U';
+          break;
+        case NIL:
+          c = '_';
+          break;
+        default:
+          printf("unsupported direction %c\n", s->direction);
+      }
+      printf("%c", c);
+    }
+    printf("\n");
   }
 }
 
-void flush_stdin() {
-  int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
-  char tmp;
-  while (read(STDIN_FILENO, &tmp, 1) == 1) { }
-  fcntl(STDIN_FILENO, F_SETFL, flags);
-}
+// ============================================================================
+
 
 void main_loop() {
   struct pollfd fds[1];
@@ -218,8 +308,7 @@ void main_loop() {
   init_empty_board(&b, 20, 10);
   //b.map[0][0] = Snake;
   SnakeData snake;
-  init_snake(&snake);
-  init_snake_on_board(&b, &snake);
+  init_snake(&snake, &b);
 
   for(;;) { 
     clear_screen();
@@ -236,16 +325,19 @@ void main_loop() {
       if(c == 'w' || c == 's' || c == 'a' || c == 'd') set_snake_direction(c, &snake);
       if(c == 'q') {
         print_board(&b);
+        //print_snake_directions(&snake, &b);
         break;
       }
     }
 
-    update_snake(&b, &snake);
+    update_snake(&snake, &b);
 
     print_board(&b);
+    //print_snake_directions(&snake, &b);
     nanosleep(&ts, NULL);
   }
 
+  free_snake(&snake, b.size_y);
   free_board(&b);
 }
 
@@ -253,20 +345,8 @@ void main_loop() {
 // ============================================================================
 
 int main() {
-  //set_input_mode(0);
   set_input_mode(1);
   main_loop();
-
-  /*pthread_t main_loop_thread, input_thread;
-  int ml_id = 1, inp_id = 1;
-
-  pthread_create(&input_thread, NULL, input_loop, &inp_id);
-  pthread_create(&main_loop_thread, NULL, main_loop_2, &ml_id);
-
-  pthread_join(main_loop_thread, NULL);
-  pthread_join(input_thread, NULL);*/
-
   set_input_mode(0);
-
   return 0;
 }
